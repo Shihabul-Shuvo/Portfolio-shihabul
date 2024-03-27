9136dbc5d1bb867e5d021ee6a31768bd6ff91694
Shihabul-Shuvo <shihabul900@gmail.com>
2024-03-27 07:48:17 +0600

    Uploaded

---
+++ data.js
@@ -0,0 +1,152 @@
+export const bio = [
+    "This is MD Shihabul Islam Shovo.",
+
+    'I’m an enthusiastic Computer Science and Engineering graduate with a solid foundation in algorithms, data structures, and object-oriented programming principles. Experienced in computer graphics programming, with a strong understanding of rendering techniques and graphics libraries such as WebGL. I’m excited to apply my skills to working on impactful projects.',
+    
+    "Thanks for taking the time to learn a little about me!"
+];
+
+export const skills = [
+    {
+        title: "Languages",
+        skillName: "C, C++, C#, Python, Java, JavaScript, HTML, WebGL, SQL",
+    },
+    {
+        title: "Frameworks/Libraries",
+        skillName: "Bootstrap, ASP.NET MVC, Unity, Three.JS",
+    },
+    {
+        title: "Design",
+        skillName: "CSS, Bootstrap, LaTex Overleaf",
+    },
+    {
+        title: "Version Control",
+        skillName: "GitHub",
+    },
+    {
+        title: "IDE",
+        skillName: "VS Code, NetBeans IDE, Codeblocks",
+    },
+];
+
+export const projects = {
+    graphicsDesignProjects: [
+        {
+            projectName: "3D Graphics Design: A Reading Room",
+            summary:
+                "A table, a chair, some books and a pen holder were designed illuminating light and shadow effect using ThreeJs and WebGL.",
+            preview: "https://github.com/Shihabul-Shuvo",
+            techStack: ["Three.js", "JavaScript", "WebGL"],
+            github: "https://github.com/Shihabul-Shuvo/Reading-Room-ThreeJs-Project",
+        },
+    ],
+    webProjects: [
+        {
+            projectName: "Easy Home",
+            summary:
+                "A house rental website where Tenants can search for rents, manage requests, and terminate agreements, while Owners can manage tenants, post ads, accept requests, and rate tenants. Common functions include communication, user rating, and writing reviews..",
+            preview: "https://github.com/Shihabul-Shuvo/EasyHome",
+            techStack: ["ASP.NET MVC", "C#", "MSSQL Server"],
+            github: "https://github.com/Shihabul-Shuvo/EasyHome",
+        },
+        {
+            projectName: "Lift",
+            summary:
+                "An e-commerce website facilitates online shopping with features for users to add multiple products to their cart and make purchases, alongside an admin panel for website management..",
+            preview: "https://github.com/Shihabul-Shuvo/Lift",
+            techStack: ["PHP", "Bootstrap", "CSS", "Mysql", "Xampp","phpMyAdmin"],
+            github: "https://github.com/Shihabul-Shuvo/Lift",
+        },
+    ],
+    softwareProjects: [
+        {
+            projectName: " Accident Reports",
+            summary:
+                "A desktop software to report accidents. Developed in Java using javaSwing for UI and MSSQL database. Jquery was used to connect to MSSQL",
+            preview: "https://github.com/Shihabul-Shuvo/Accident-Reports",
+            techStack: ["Bootstrap", "JavaScript", "AWS-S3"],
+            github: "https://github.com/Shihabul-Shuvo/Accident-Reports",
+        },
+    ],
+};
+
+export const experience = [
+    
+];
+
+export const education = [
+    {
+        title: "B.Sc. in Computer Science and Engineering",
+        duration: "2019 - 2024",
+        subtitle: "Ahsamullah University of Science and Technology, Tejgaon, Dhaka",
+        details: ["CGPA: 3.131 out of 4"],
+        tags: [
+            "Data Structures & Algorithms",
+            "Operating Systems",
+            "Database Management System",
+            "Networking",
+            "Formal Language and Compiler",
+            "Web Design",
+            "Computer Graphics",
+        ],
+        icon: "graduation-cap",
+    },
+    {
+        title: "Higher Secondary Certificate (HSC)",
+        duration: "2016 - 2018",
+        subtitle: "Cantonment Public School and College, Saidpur, Nilphamari",
+        details: ["GPA: 5 out of 5"],
+        tags:[],
+        icon: "book",
+    },
+    {
+        title: "Secondary School Certificate (SSC)",
+        duration: "2014 - 2016",
+        subtitle: "Cantonment Public School and College, Saidpur, Nilphamari",
+        details: ["GPA: 5 out of 5"],
+        tags:[],
+        icon: "book",
+    },
+];
+
+export const footer = [
+    {
+        label: "Dev Profiles",
+        data: [
+            {
+                text: "GitHub",
+                link: "https://github.com/Shihabul-Shuvo",
+            },
+            {
+                text: "LeetCode",
+                link: "",
+            },
+        ],
+    },
+    {
+        label: "Resources",
+        data: [
+            {
+                text: "Enable Dark/Light Mode",
+                func: "enableDarkMode()",
+            },
+            {
+                text: "Print this page",
+                func: "window.print()",
+            },
+        ],
+    },
+    {
+        label: "Social Profiles",
+        data: [
+            {
+                text: "Linkedin",
+                link: "https://www.linkedin.com/in/vinaysomawat/",
+            },
+            {
+                text: "Facebook",
+                link: "https://www.facebook.com/shihabul.shovo",
+            },
+        ],
+    },
+];
